87b3eb0001c17b27f303f5c1a417458ccd7755a3
Becominging <1312545407@qq.com>
2022-11-22 12:03:34 +0000

    delete

--- organization-service/src/main/java/net/javaguides/organizationservice/service/OrganizationService.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.organizationservice.service;
-
-import net.javaguides.organizationservice.dto.OrganizationDto;
-
-public interface OrganizationService {
-    OrganizationDto saveOrganization(OrganizationDto organizationDto);
-
-    OrganizationDto getOrganizationByCode(String organizationCode);
-}

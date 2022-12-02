864afd1dd979aee8738d563cb42b7c95149d4cc5
Yan <liuyan_zju@163.com>
2022-12-02 20:29:26 +0000

    Enable Eureka

--- distributed-project/employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ distributed-project/employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -6,19 +6,12 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
-//@EnableEurekaClient
+@EnableEurekaClient
 public class EmployeeServiceApplication {
-
-//	@Bean
-//	@LoadBalanced
-//	public RestTemplate restTemplate(){
-//		return new RestTemplate();
-//	}
 
 	@Bean
 	@LoadBalanced
